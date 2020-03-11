cf75160e0268c99c253408aa50e6b8209a334ee8
mad-cunt <me_@live.se>
2020-03-11 09:40:12 +0100

    Updates

---
+++ precache-manifest.0c33ee9cd6018e9513f25748c6bf4f98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f86c6a9d212e5bb201e059f7434d592",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "9311a4f89a6cbc2556a6",
+    "url": "/smart-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9982ee38334e3fdd1185",
+    "url": "/smart-brain/static/css/main.ae9cf29a.chunk.css"
+  },
+  {
+    "revision": "9311a4f89a6cbc2556a6",
+    "url": "/smart-brain/static/js/2.a3c0e962.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/smart-brain/static/js/2.a3c0e962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9982ee38334e3fdd1185",
+    "url": "/smart-brain/static/js/main.a4e74682.chunk.js"
+  },
+  {
+    "revision": "e68a7aa19c1e5c2a2d86",
+    "url": "/smart-brain/static/js/runtime-main.4dc2cd4d.js"
+  }
+]);
